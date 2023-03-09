8eb2a7bff27290dfa933364f991869a858280f32
Steven DeTeso <sadeteso@gmail.com>
2023-03-08 20:08:16 -0500

    react app simple page started, navbar written

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function NavBar () {
+    return (
+        <nav className="navbar">
+            <img src="../images/reactjs-icon.png" alt="react img"/>
+            <h3>ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
+
+export default NavBar
